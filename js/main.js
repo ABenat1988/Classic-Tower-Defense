913bcc06c8966f2e4d350f4a1c772b688a55a7c1
ABenat1988 <ale.benat@gmail.com>
2023-03-08 14:29:56 +0100

    tower placement done, all design done, 1 bug to solve before mvp done

--- js/main.js
+++ js/main.js
@@ -8,15 +8,8 @@ class Game {
     start() {
         this.map = new Map(wayPointsLevel1, towerPlacementLevel1);
 
-        /*const towerTest = new Tower(1, 200, 10, 23, 8);
-        this.towerArray.push(towerTest);
-        const towerTest2 = new Tower(1, 10, 5, 5, 12);
-        this.towerArray.push(towerTest2);
-        const towerTest3 = new Tower(1, 200, 10, 39, 47);
-        this.towerArray.push(towerTest3);*/
-
         setInterval(() => {
-            const monster = new Monster("soldier", 5, 1, 1, this.map.wayPoint[0].x, this.map.wayPoint[0].y);
+            const monster = new Soldier(this.map.wayPoint[0].x, this.map.wayPoint[0].y);
             this.monsterArray.push(monster);
         }, 1750);
 
@@ -28,7 +21,7 @@ class Game {
         }, 20);
     }
     removeMonster(monsterInstance) {
-        if (this.map.wayPoint[this.map.wayPoint.length - 1].x * 32 === monsterInstance.positionX && this.map.wayPoint[this.map.wayPoint.length - 1].y * 32 === monsterInstance.positionY) {
+        if (this.map.wayPoint[this.map.wayPoint.length - 1].x * this.map.squareDimension + this.map.squareDimension / 2 === monsterInstance.positionX && this.map.wayPoint[this.map.wayPoint.length - 1].y * this.map.squareDimension + this.map.squareDimension / 2 === monsterInstance.positionY) {
             monsterInstance.monsterElm.remove();
             this.monsterArray.shift();
         }
@@ -48,22 +41,24 @@ class Map {
         this.towerPlacement.forEach((element) => {
             const divTower = document.createElement('div');
             divTower.setAttribute("class", "placement");
-            divTower.style.left = element.x * 32 + "px";
-            divTower.style.bottom = element.y * 32 + "px";
+            divTower.dataset.positionX = element.x;
+            divTower.dataset.positionY = element.y;
+            divTower.style.left = element.x * this.squareDimension + "px";
+            divTower.style.bottom = element.y * this.squareDimension + "px";
             document.getElementById("towers-placement").appendChild(divTower);
         });
     }
 }
 
 class Monster {
-    constructor(type, health, speed, attack, positionX, positionY) {
+    constructor(type, health, attack, width, positionX, positionY) {
         this.type = type;
         this.health = health;
-        this.speed = speed;
         this.attack = attack;
         this.monsterElm = null;
-        this.positionX = positionX * 32;
-        this.positionY = positionY * 32;
+        this.positionX = positionX * game.map.squareDimension + game.map.squareDimension / 2;
+        this.positionY = positionY * game.map.squareDimension + game.map.squareDimension / 2;
+        this.width = width;
         this.movementDirection = null;
         this.createDomElementMonster();
     }
@@ -74,8 +69,8 @@ class Monster {
             case 'soldier':
                 this.monsterElm.setAttribute("class", "monster soldier");
                 break;
-            case 'speeder':
-                this.monsterElm.setAttribute("class", "monster speeder");
+            case 'tank':
+                this.monsterElm.setAttribute("class", "monster tank");
                 break;
             case 'boss':
                 this.monsterElm.setAttribute("class", "monster boss");
@@ -83,37 +78,60 @@ class Monster {
             default:
                 this.monsterElm.setAttribute("class", "error");
         }
-        this.monsterElm.style.left = this.positionX + "px";
-        this.monsterElm.style.bottom = this.positionY + "px";
+        this.monsterElm.style.left = this.positionX - this.width / 2 + "px";
+        this.monsterElm.style.bottom = this.positionY - this.width / 2 + "px";
         document.getElementById("monsters").appendChild(this.monsterElm);
     }
     moveMonster() {
         game.map.wayPoint.forEach((waypoint) => {
-            if (waypoint.x * 32 === this.positionX && waypoint.y * 32 === this.positionY) {
+            if (waypoint.x * game.map.squareDimension + game.map.squareDimension / 2 === this.positionX && waypoint.y * game.map.squareDimension + game.map.squareDimension / 2 === this.positionY) {
                 this.movementDirection = waypoint.newDirection;
             }
         });
         switch (this.movementDirection) {
             case 'top':
                 this.positionY = this.positionY + 1;
-                this.monsterElm.style.bottom = this.positionY + "px";
+                this.monsterElm.style.bottom = this.positionY - this.width / 2 + "px";
                 break;
             case 'bottom':
                 this.positionY = this.positionY - 1;
-                this.monsterElm.style.bottom = this.positionY + "px";
+                this.monsterElm.style.bottom = this.positionY - this.width / 2 + "px";
                 break;
             case 'left':
                 this.positionX = this.positionX - 1;
-                this.monsterElm.style.left = this.positionX + "px";
+                this.monsterElm.style.left = this.positionX - this.width / 2 + "px";
                 break;
             case 'right':
                 this.positionX = this.positionX + 1;
-                this.monsterElm.style.left = this.positionX + "px";
+                this.monsterElm.style.left = this.positionX - this.width / 2 + "px";
                 break;
             default:
         }
     }
 }
+class Soldier extends Monster {
+    constructor(positionX, positionY) {
+        super('soldier', 5, 1, 20, positionX, positionY);
+    }
+}
+class Tank extends Monster {
+    constructor(positionX, positionY) {
+        super('tank', 20, 2, 26, positionX, positionY);
+    }
+}
+class Boss extends Monster {
+    constructor(positionX, positionY) {
+        super('boss', 15, 5, 32, positionX, positionY);
+    }
+}
+
+
+
+
+
+
+
+
 
 class Tower {
     constructor(power, rateOfFire, range, positionX, positionY) {
@@ -125,8 +143,9 @@ class Tower {
         this.isTargetAcquired = false;
         this.target = null;
 
-        this.positionX = positionX * 32;
-        this.positionY = positionY * 32;
+        this.positionX = positionX * game.map.squareDimension + game.map.squareDimension;
+        this.positionY = positionY * game.map.squareDimension + game.map.squareDimension;
+        this.width = 64;
         this.towerElm = null;
 
         this.createDomElementTower();
@@ -136,8 +155,8 @@ class Tower {
     createDomElementTower() {
         this.towerElm = document.createElement('div');
         this.towerElm.setAttribute("class", "tower");
-        this.towerElm.style.left = this.positionX + "px";
-        this.towerElm.style.bottom = this.positionY + "px";
+        this.towerElm.style.left = this.positionX - this.width / 2 + "px";
+        this.towerElm.style.bottom = this.positionY - this.width / 2 + "px";
         document.getElementById("towers").appendChild(this.towerElm);
         //this.displayRange()
     }
@@ -197,8 +216,8 @@ class Tower {
                 } else {
                     projectileElement.positionY--;
                 }
-                projectileElement.projectileElm.style.bottom = projectileElement.positionY + "px";
-                projectileElement.projectileElm.style.left = projectileElement.positionX + "px";
+                projectileElement.projectileElm.style.bottom = projectileElement.positionY - projectileElement.width / 2 + "px";
+                projectileElement.projectileElm.style.left = projectileElement.positionX - projectileElement.width / 2 + "px";
                 this.setDamage(projectileElement);
             });
         }, 1);
@@ -226,22 +245,27 @@ class Tower {
 
 }
 
+
+
+
+
 class Projectile {
-    constructor(sourceTower, target, damage) {
+    constructor(sourceTower, target) {
         this.sourceTower = sourceTower;
         this.positionX = sourceTower.positionX;
         this.positionY = sourceTower.positionY;
         this.projectileElm = null;
+        this.width = 10;
         this.target = target;
-        this.damage = damage;
+        this.damage = sourceTower.power;
         this.createDomElementProjectile();
     }
 
     createDomElementProjectile() {
         this.projectileElm = document.createElement('div');
         this.projectileElm.setAttribute("class", "projectile");
-        this.projectileElm.style.left = this.positionX + "px";
-        this.projectileElm.style.bottom = this.positionY + "px";
+        this.projectileElm.style.left = this.positionX - this.width / 2 + "px";
+        this.projectileElm.style.bottom = this.positionY - this.width / 2 + "px";
         document.getElementById("projectiles").appendChild(this.projectileElm);
     }
 }
@@ -442,7 +466,10 @@ const game = new Game();
 game.start();
 
 window.addEventListener("click", (evt) => {
-    console.log(evt.target.style.left.slice(0, -2)*1/32);
-    console.log(evt.target.style.bottom.slice(0, -2)*1/32);
-    game.towerArray.push(new Tower(1, 10, 5, evt.target.style.left.slice(0, -2)*1/32, evt.target.style.bottom.slice(0, -2)*1/32));
+    console.log(evt.target.dataset.positionX);
+    console.log(evt.target.dataset.positionY);
+    
+    if (evt.target.className === "placement") {
+        game.towerArray.push(new Tower(1, 10, 5, evt.target.dataset.positionX, evt.target.dataset.positionY));
+    }
 });
